ebecd0d22b357d48b5f1cd6ecdeb2f9c993049e0
Monica Kim <monica606@naver.com>
2025-07-16 02:16:50 -0700

    fix: update DevSetup model

--- otaku-stream-vss-app/server/models/DevSetup.cjs
+++ otaku-stream-vss-app/server/models/DevSetup.cjs
@@ -1,6 +1,7 @@
 const { Logging } = require('../server-logging.cjs');
 const { uploadsDev } = require('../server-uploads.cjs');
 const { Member } = require('./Accounts/member.cjs');
+const { Session } = require('./Accounts/session.cjs');
 const { Anime } = require('./Anime/Anime.cjs');
 const { AnimeInstallment } = require('./Anime/AnimeInstallment.cjs');
 const { Genre } = require('./Anime/Genre.cjs');
@@ -9,12 +10,30 @@ const { AnimeFavorite } = require('./Anime/AnimeFavorite.cjs');
 const { AnimeStream } = require('./Anime/AnimeStream.cjs');
 const { AnimeStreamLike } = require('./Anime/AnimeStreamLike.cjs');
 const { AnimeWatchHistory } = require('./Anime/AnimeWatchHistory.cjs');
+const { BillingHistory } = require('./BillingHistory.cjs');
+const { Premium } = require('./Premium.cjs');
+const { SavedCard } = require('./SavedCard.cjs');
 const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 async function DevSetup()
 {
     try
     {
+        // Force sync all models to create tables
+        await Member.sync({ force: true });
+        await Session.sync({ force: true });
+        await Anime.sync({ force: true });
+        await Genre.sync({ force: true });
+        await AnimeGenre.sync({ force: true });
+        await AnimeFavorite.sync({ force: true });
+        await AnimeInstallment.sync({ force: true });
+        await AnimeStream.sync({ force: true });
+        await AnimeStreamLike.sync({ force: true });
+        await AnimeWatchHistory.sync({ force: true });
+        await BillingHistory.sync({ force: true });
+        await Premium.sync({ force: true });
+        await SavedCard.sync({ force: true });
+
         const chanMember = await Member.AddToDB('chandler@gmail.com', 'password');
         const JonMember = await Member.AddToDB('jongmin@gmail.com', 'password');
         const MonMember = await Member.AddToDB('monica@gmail.com', 'password');
