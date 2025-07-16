4e6630ba96730ccb52b16a8bbf8c115c634e2502
Monica Kim <monica606@naver.com>
2025-07-16 02:16:52 -0700

    fix: update API router

--- otaku-stream-vss-app/server/routes/api/api-router.cjs
+++ otaku-stream-vss-app/server/routes/api/api-router.cjs
@@ -3,6 +3,9 @@ const apiRouter = express.Router()
 const authentifyRouter = require('./authentify-router.cjs');
 const authorizeRouter = require('./authorize-router.cjs');
 const animeRouter = require('./anime/anime-router.cjs');
+const billingRouter = require('./billing-router.cjs');
+const premiumRouter = require('./premium-router.cjs');
+const savedCardRouter = require('./saved-card-router.cjs');
 
 // simple api example route
 apiRouter.get('/Example', (req, res) => {
@@ -14,5 +17,11 @@ apiRouter.use('/authentify', authentifyRouter);
 apiRouter.use('/authorize', authorizeRouter);
 
 apiRouter.use('/anime', animeRouter);
+
+apiRouter.use('/billing', billingRouter);
+
+apiRouter.use('/premium', premiumRouter);
+
+apiRouter.use('/saved-cards', savedCardRouter);
 
 module.exports = apiRouter;
