3714d73e0418d0edd8c8bb5f314e861ba2453b84
Monica Kim <monica606@naver.com>
2025-07-16 02:12:11 -0700

    fix: update main entry point

--- otaku-stream-vss-app/client/src/main.jsx
+++ otaku-stream-vss-app/client/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import 'bootstrap/dist/css/bootstrap.min.css' //[Import bootstrap css in your own files to use it]
-//import '../tailwind.css'    //[Import tailwind css in your own files to use it]
+import './tailwind.css'    //[Import tailwind css in your own files to use it]
+import './pages/forgotpassword/ForgotPassword.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
